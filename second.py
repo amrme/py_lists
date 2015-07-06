61b4ce3c6187f5bb505a2d6c2c8cca19c15f01e7
Amr <amr.e.dev@gmail.com>
2015-07-06 03:55:35 +0200

    fixing it

--- second.py
+++ second.py
@@ -1,7 +1,19 @@
 fname = raw_input("Enter file name: ")
-if len(fname) < 1 : fname = "mbox-short.txt"
+if len(fname) < 1 : fname = "mbox-short.txt" #good line, bad comment!
 
 fh = open(fname)
 count = 0
+for line in fh:
+	if line == "":  
+		continue
+
+	words = line.split()
+
+	for word in words:
+		if word is not "From: ":  
+			continue
+
+		count += 1
+		print words[2]
 
 print "There were", count, "lines in the file with From as the first word"
