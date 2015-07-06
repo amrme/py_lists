e9e6e2c56db2629259d82fd3e67832dfaf810a7b
Amr <amr.e.dev@gmail.com>
2015-07-06 03:44:10 +0200

    submitted

--- one.py
+++ one.py
@@ -1,12 +1,11 @@
-
 # read a file
 # split it up into words
 # append all new words in the file to a list
 # sort that list
 # print that sorted list
 
-# fname = raw_input("Enter file name: ")
-fh = open("./romeo.txt")
+fname = raw_input("Enter file name: ")
+fh = open(fname)
 lst = list()
 
 for line in fh:
@@ -14,17 +13,17 @@ for line in fh:
     listed_line = line.split()
     # print listed_line
     for word in listed_line:
-    	# print word
-      	if word in lst: 
-      		# print "yuo"
-      		continue
+        # print word
+        if word in lst:
+                # print "yuo"
+                continue
 
-    	lst.append(word)
-    	# print "appended"
-    	# print lst
+        lst.append(word)
+        # print "appended"
+        # print lst
 
 
-      
+
 sorted_list = sorted(lst)
 
 print sorted_list
